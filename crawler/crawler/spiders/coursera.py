179af70371288ffee900ad7e211ba8cecc6beff3
AINext <admin@ainext.asia>
2018-01-01 06:28:03 +0000

    coursera course name parsing

---
+++ crawler/crawler/spiders/coursera.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+class CourseraSpider(scrapy.Spider):
+    name = 'coursera'
+    allowed_domains = ['coursera.org']
+    start_urls = [
+                  # 'http://coursera.org/browse/computer-science',
+                  'https://www.coursera.org/browse/computer-science/software-development',
+                 ]
+
+    def parse(self, response):
+        element = response.css(".rc-BrowseSummary")
+        yield {
+            'title' :  element.xpath("//h1/text()").extract_first(),
+        }
+
+        courses = response.xpath("//div[@class='offering-content']//h2/text()")
+        for course in courses:
+            yield {
+                'course' : course.extract(),
+            }
+
+        next_page = response.css("a[data='right-arrow']::attr('href')").extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+        pass
